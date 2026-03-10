b55e3109965854df863d74554563a6a0a5b12341
Oyama <oysmbatani@gmail.com>
2026-03-10 18:58:46 +0200

    End of section 3

---
+++ Client/src/lib/types/index.d.ts
@@ -0,0 +1,12 @@
+type Activity = {
+  id: string;
+  title: string;
+  description: string;
+  category: string;
+  date: string;
+  isCancelled: boolean;
+  venue: string;
+  city: string;
+  longitude: number;
+  latitude: number;
+};
